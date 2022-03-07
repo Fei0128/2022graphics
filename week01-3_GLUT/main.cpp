aaf3d2bace1486ae278a353bd92a8cb208a37934
Fei0128 <b36383201@gmail.com>
2022-03-07 16:48:25 +0800

    modify week03

--- week01-3_GLUT/main.cpp
+++ week01-3_GLUT/main.cpp
@@ -4,8 +4,13 @@ void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glColor3f(1,1,0);
-    glutSolidTeapot( 0.3 );
+    glBegin(GL_TRIANGLES);
+
+        glColor3f(1.0f, 0.0f, 0.0f);   glVertex2f(0.0f,   1.0f);
+        glColor3f(0.0f, 1.0f, 0.0f);   glVertex2f(0.87f,  -0.5f);
+        glColor3f(0.0f, 0.0f, 1.0f);   glVertex2f(-0.87f, -0.5f);
+
+    glEnd();
 
     glutSwapBuffers();
 }
